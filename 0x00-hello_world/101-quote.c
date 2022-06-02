5584bb81a6a95a87ae1c893a19b09aab3ba3ddf0
1111Dawit <dawitgetachew1613@gmail.com>
2022-06-02 02:33:57 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - Entry point
+ * main - Prints out the last part of a quote in the standard error.
  *
- * Return: Always 0 (Success)
+ * Return:  1 if success
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\n" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\n" - Dora Korpar, 2015-10-19\n", 58);
 return (1);  
 }
