63e515b3f086a2aac23807d8e34f8f6f944bee2f
1111Dawit <dawitgetachew1613@gmail.com>
2022-06-03 04:43:21 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,12 +9,12 @@ int main(void)
 char c;
 for (c = 'a'; c <= 'z'; c++)
 {
-putchar(c);     
+putchar (c);     
 }
 for (c = 'A'; c <= 'Z'; c++)
 {
 putchar(c);
 }
- putchar('\n');
- return (0);
+putchar('\n');
+return (0);
 }
