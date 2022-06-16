661cd64b08cfc34243b74198b5d6cb839657292e
1111Dawit <dawitgetachew1613@gmail.com>
2022-06-16 13:44:53 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,8 +4,7 @@
 * @dest: The strings to be appended upon.
 * @n: The number of bytes from src to be appended to dest.
 */
-char *_strncat(char *dest,char *src,int n)
-{
+char *_strncat(char *dest, char *src, int n)
 int index = 0, dest_len = 0;
 while (dest[index++])
 dest_len++;
