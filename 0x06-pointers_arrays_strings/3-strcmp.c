ecd2bda7673e02278593c95458a648d0d4bc3390
1111Dawit <dawitgetachew1613@gmail.com>
2022-06-16 14:21:10 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
 * _strcmp - a function that compares two strings
-* @s1: input one and @s2: input two
+* @s1: input one
+* @s2: input two
 * Return: Always 0 (Success)
 */
 int _strcmp(char *s1, char *s2)
@@ -10,7 +11,7 @@ int i;
 for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 {
 if (s1[i] != s2[i])
-return(s1[i] - s2[i]);
+return (s1[i] - s2[i]);
 }
-return(0);
+return (0);
 }
