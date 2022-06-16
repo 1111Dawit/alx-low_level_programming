ee37ecf2233fc2736686ff4799aa264d642f1622
1111Dawit <dawitgetachew1613@gmail.com>
2022-06-16 13:33:22 -0700

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* _strncat -concatenate two strings
+* @dest: The strings to be appended upon.
+* @n: The number of bytes from src to be appended to dest.
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
+return (dest);
+}
