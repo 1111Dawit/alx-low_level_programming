c25c9d023c5984ed810bc20831debb98025774f3
1111Dawit <dawitgetachew1613@gmail.com>
2022-06-16 15:01:36 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * string_toupper - a function that chages all lower string to upper 
+ * @n: input string
+ * Return: capitalized string
+ */
+char *string_toupper(char *n)
+{
+  int i;
+  for (i = 0; n[i] != '\0'; i++)
+    {
+      if (n[i] >= 'a' && n[i] <= 'z')
+	n[i] = n[i] - 32;
+	  }
+  return (n);
+}
