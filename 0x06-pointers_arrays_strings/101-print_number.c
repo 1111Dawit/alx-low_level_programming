c55abedacf14eef84aded937cb61629daf071a6e
1111Dawit <dawitgetachew1613@gmail.com>
2022-06-17 00:25:05 -0700

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,13 +5,14 @@
 */
 void print_number(int n)
 {
-unsigned int num = n;
+unsigned int x;
 if (n < 0)
 {
 _putchar('-');
-num = -num;
+num *= -1;
 }
-if ((num / 10) > 0)
-print_number(num / 10);
-_putchar((num % 10) + '0');
+x = n;
+ if (x / 10)
+print_number(x / 10);
+_putchar((x % 10) + '0');
 }
