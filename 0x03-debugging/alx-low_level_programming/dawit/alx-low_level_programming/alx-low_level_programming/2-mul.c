34eba9de5661a9ab759462b3119678021a3b7bc9
1111Dawit <dawitgetachew1613@gmail.com>
2022-06-21 23:32:26 -0700

    Task 0

---
+++ 0x03-debugging/alx-low_level_programming/dawit/alx-low_level_programming/alx-low_level_programming/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* mul - Multiples, two intigers
+* @x: The first number to multiply
+* @y: The second number to multiply with first number
+*
+* Return: The result of multiplication between x and y param
+*/
+int mul(int x, int y)
+{
+return(x * y)
+}
