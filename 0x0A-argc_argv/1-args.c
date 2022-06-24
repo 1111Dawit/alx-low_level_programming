45ce405bf93c58a6022185de953678ff9cd0e53e
1111Dawit <dawitgetachew1613@gmail.com>
2022-06-24 13:00:15 -0700

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
 *
 * Return: Always 0
 */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 printf("%\d\n", argc -1);
 return (0);
