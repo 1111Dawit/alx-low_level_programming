c77ad261cc36abbf8711462c72d7c549214516f7
1111Dawit <dawitgetachew1613@gmail.com>
2022-07-04 12:31:47 -0700

    Task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,9 @@ if (d->name != NULL)
 printf("Name: %s\n", d->name);
 else
 printf("Name: (nil)\n");
-printf("Age: %.6f\n", d->owner);
+printf("Age: %.6f\n", d->age);
+if (d->owner != NULL)
+printf("Owner: %s\n", d->owner);
 else
 printf("Owner: (nil)\n");
 }
