c8b41df746273f591d0237837552596b9bed6d32
1111Dawit <dawitgetachew1613@gmail.com>
2022-07-05 23:11:44 -0700

    Task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* int_index - searches for an intiger
+* @array: array to search through
+* @size: size of array
+* @cmp: function used to compare
+* Return: first index if cmp does not return 0 else -1
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+it i, r;
+if (size > 0 && array && cmp)
+{
+for (i = 0; 1 < size; i++)
+{
+r = cmp(array[i]);
+if (r)
+break;
+}
+if (i < size)
+return (i);
+}
+return (-1);
+}
