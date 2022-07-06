0b5409b12885b66800cd50ebe2217d121674f726
1111Dawit <dawitgetachew1613@gmail.com>
2022-07-05 23:53:57 -0700

    Task 3.2

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,48 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* op_add - adds a and b
+* @a: first number
+* @b: second number
+* Return: sum of a and b
+*/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+* op_sub -substructs b from a
+* @a: first number
+* @b: second numer
+* Return: difference fo a and b
+*/
+int op_sub(int a, int b)
+{
+return (a -b);
+}
+/**
+* op_mul - multiples a and b
+* @a: first number
+* @b: second number
+* Return: nmultiplication of a and b
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+* op_div - divides a by b
+* @a: first number
+* @b: second number
+* Return: remainder of the division of a by b
+*/
+int op_mod(int a, int b)
+{
+if (b == 0)
+{
+printf("ERROR\n");
+exit(100);
+}
+return (a % b);
+}
