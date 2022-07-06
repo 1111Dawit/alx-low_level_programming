4d6ffb789df4b1268d71828e1c756b7983a390bd
1111Dawit <dawitgetachew1613@gmail.com>
2022-07-06 09:51:24 -0700

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "function_pointers.h"
 /**
 * int_index - searches for an intiger
 * @array: array to search through
@@ -9,17 +8,13 @@
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int i, r;
-if (size > 0 && array && cmp)
+int index;
+if (array == NULL || cmp == NULL)
+return (-1);
+for (index = 0; index < size; index++)
 {
-for (i = 0; 1 < size; i++)
-{
-r = cmp(array[i]);
-if (r)
-break;
-}
-if (i < size)
-return (i);
+if (cmp(array[index]) != 0)
+return (index);
 }
 return (-1);
 }
