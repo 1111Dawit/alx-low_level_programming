4825e67ac71ddd78f0fcf467d58a7343a0752983
1111Dawit <dawitgetachew1613@gmail.com>
2022-07-07 04:50:11 -0700

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,6 +3,7 @@
 /**
 * sum_them_all - sums of all its parameters.
 * @n: number of parameters
+* @...: A variable number of all parameters
 * Return: sum of all parameters
 */
 int sum_them_all(const unsigned int n, ...)
