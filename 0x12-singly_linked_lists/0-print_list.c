891f6acd7dd6d21b8315b72a3c4b81370e08fa49
1111Dawit <dawitgetachew1613@gmail.com>
2022-07-14 05:29:48 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,15 +13,15 @@ if (h == NULL)
 return (0);
 while (h != NULL)
 {
-if (h->str == NULL)
+if (h -> str == NULL)
 {
 printf("[%d] %s\n", 0, "(nil)");
 }
 else
 {
-printf("[%d] %s\n", h->len, h->str);
+printf("[%d] %s\n", h -> len, h -> str);
 }
-h = h->next;
+h = h -> next;
 count++;
 }
 return (count);
