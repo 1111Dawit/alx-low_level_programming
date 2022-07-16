c0b3b5637c5a00e005ea018e7cb83d768de0e86c
1111Dawit <dawitgetachew1613@gmail.com>
2022-07-16 01:12:38 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+/**
+* print_alphabet - a function that prints the alphabet, in lowercase
+* Return: always 0 (Success)
+*/
+void print_alphabet(void)
+{
+int n;
+for (n = 'a'; n <= 'z'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+}
