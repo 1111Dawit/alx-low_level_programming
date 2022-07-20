b575d6cc30db579d992da21a726aefc0e2e451de
1111Dawit <dawitgetachew1613@gmail.com>
2022-07-19 19:26:13 -0700

    Task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,11 +1,10 @@
 #include "lists.h"
-#inlcude <stdlib.h>
 /**
 * free_listint2 - free's memory
 * @head: head
 * Return: 0
 */
-void free_list2(listint_t **head)
+void free_listint2(listint_t **head)
 {
 listint_t *next;
 if (head == NULL || *head == NULL)
