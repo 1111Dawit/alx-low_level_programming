a21bf68b3cd6b8f3d7a6d319b7769039deb2a92d
1111Dawit <dawitgetachew1613@gmail.com>
2022-07-19 19:43:22 -0700

    Task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,14 +7,14 @@
 */
 void free_listint2(listint_t **head)
 {
-  listint_t *first;
-  int n;
-  if (head == NULL || *head == NULL)
-    return (0);
-  first = *head;
-  *head = first->next;
-  n = first->n;
-  free(first);
-  return ();
+listint_t *first;
+int n;
+if (head == NULL || *head == NULL)
+return (0);
+first = *head;
+*head = first->next;
+n = first->n;
+free(first);
+return (n);
 }
 }
