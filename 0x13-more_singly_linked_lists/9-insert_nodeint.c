14bed7fa6827785f9e9564df14d082f228c8fa36
1111Dawit <dawitgetachew1613@gmail.com>
2022-07-19 21:04:18 -0700

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
     {
       ptr->next = NULL;
     }
-  if (head == NULL || idx == 0)
+  if (*head == NULL || idx == 0)
     {
       ptr->next = *head;
       *head = ptr;
