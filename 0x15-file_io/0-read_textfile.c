9dbb43dbd1da97fd540214923f433174b459ee55
1111Dawit <dawitgetachew1613@gmail.com>
2022-07-25 03:45:04 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,21 +12,27 @@ ssize_ read_textfile(const char *filename, size_t letters)
 int file;
 int length, wrotechars;
 char *buf;
-if (filename == NULL || letterss == 0)
+if (filename == NULL || letters == 0)
 return (0);
 buf = malloc(sizeof(char) * (letters));
 if (buf == NULL)
 return (0);
-file = open(filename, 0_RDONLY);
+file = open(filename, O_RDONLY);
 if (file == -1)
 {
 free(buf);
-close(file);
 return (0);
 }
-wrotechars = write(STDOUT_FILENO, buf, length);
+ length = read(file, buf, letters);
+ if (length == -1)
+   {
 free(buf);
 close(file);
+ return (0);
+}
+ wrotechars = write(STDOUT_FILENO, buf, length);
+ free(buf);
+ close(file);
 if (wrotechars != length)
 return (0);
 return (length);
